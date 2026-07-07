efd70cf70015607378e658fcfc6056b2220892f3
Carvajalandre <andrexsantiago20@gmail.com>
2026-07-07 13:20:31 -0400

    fix: tamaño de minimalista

--- lib/widgets/minimalist_visualizer.dart
+++ lib/widgets/minimalist_visualizer.dart
@@ -15,52 +15,55 @@ class MinimalistVisualizer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final artSize = size * 0.7;
     return SizedBox(
       width: size,
       height: size,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            width: size,
-            height: size,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(size * 0.15),
-              boxShadow: [
-                BoxShadow(
-                  color: glowColor.withAlpha(50),
-                  blurRadius: 40,
-                  spreadRadius: 8,
-                ),
-              ],
+      child: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              width: artSize,
+              height: artSize,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(artSize * 0.15),
+                boxShadow: [
+                  BoxShadow(
+                    color: glowColor.withAlpha(50),
+                    blurRadius: 40,
+                    spreadRadius: 8,
+                  ),
+                ],
+              ),
             ),
-          ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(size * 0.15),
-            child: SizedBox(
-              width: size,
-              height: size,
-              child: albumId != null
-                  ? QueryArtworkWidget(
-                      id: albumId!,
-                      type: ArtworkType.ALBUM,
-                      artworkFit: BoxFit.cover,
-                      artworkWidth: size,
-                      artworkHeight: size,
-                      keepOldArtwork: true,
-                      nullArtworkWidget: _defaultArt(),
-                    )
-                  : _defaultArt(),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(artSize * 0.15),
+              child: SizedBox(
+                width: artSize,
+                height: artSize,
+                child: albumId != null
+                    ? QueryArtworkWidget(
+                        id: albumId!,
+                        type: ArtworkType.ALBUM,
+                        artworkFit: BoxFit.cover,
+                        artworkWidth: artSize,
+                        artworkHeight: artSize,
+                        keepOldArtwork: true,
+                        nullArtworkWidget: _defaultArt(artSize),
+                      )
+                    : _defaultArt(artSize),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
-  Widget _defaultArt() => Container(
+  Widget _defaultArt(double size) => Container(
         color: const Color(0xFF1C1C1C),
-        child: const Icon(Icons.music_note_rounded,
-            color: Colors.white54, size: 48),
+        child: Icon(Icons.music_note_rounded,
+            color: Colors.white54, size: size * 0.18),
       );
 }
